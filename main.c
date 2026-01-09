2bff7ea8f027009a6397b08a92aaf73c26236826
VladHlushchyk <vladglusik4@gmail.com>
2026-01-10 01:25:32 +0200

    Fixed a lot of bugs, and issues with solving. Added waiting before closing window

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 typedef struct operator{
     unsigned char op : 6; //6bits enough for 64 numbers, if you use symbol that have bigger number in ASCII, just remove ": 6", and ": 2"
-    char pr : 2; //priority of the operator. 2bits enough for numbers in range [-2; 1], enough for what we need.
+    unsigned char pr : 2; //priority of the operator. 2bits enough for numbers in range [0; 3], enough for what we need.
 } OP;
 
 typedef struct tokens{
@@ -22,37 +22,37 @@ char getC(void)
     return c;
 }
 
-double solver(TOK *tokens, int *numAm)
+double solver(TOK *tokens, int *numAm) //solving the math example, and return result
 {
     TOK temp;
-    int i, j;
-    double res;
+    double res, res_mul;
+    int i=0, j=0;
 
-    for(int i=0; i<(*numAm-1); ++i){
+    for(i; i<*numAm; ++i, ++j){
         if(tokens->ops[i].pr==1){
-            j = i;
-            res = tokens->nums[i];
+            res_mul=tokens->nums[i];
             while(tokens->ops[i].pr==1){
                 switch (tokens->ops[i].op)
                 {
-                case '*': res *= tokens->nums[++i];     break;
-                case '/': res /= tokens->nums[++i];     break;
+                case '*': res_mul *= tokens->nums[++i];     break;
+                case '/': res_mul /= tokens->nums[++i];     break;
                 }
             }
-            temp.nums[j]=res;
-            temp.ops[j].op='+';
-            temp.ops[j].pr=0;
+            temp.nums[j]=res_mul;
+            if (i < *numAm - 1) temp.ops[j] = tokens->ops[i];
+            else temp.ops[j].op = 0;
         } else{
-            temp.ops[i] = tokens->ops[i];
-            temp.nums[i] = tokens->nums[i];
+            temp.ops[j] = tokens->ops[i];
+            temp.nums[j] = tokens->nums[i];
         }
-        }
+    }
 
-    for(int u=0; u<j; ++u){
+    res = temp.nums[0];
+    for(int u=0; u<=j; ++u){
         switch(temp.ops[u].op)
         {
-            case '+': res += temp.nums[u];    break;
-            case '-': res -= temp.nums[u];    break;
+            case '+': res += temp.nums[u+1];    break;
+            case '-': res -= temp.nums[u+1];    break;
         }
     }
 
@@ -67,6 +67,8 @@ int parser(void)
     char c;
     TOK tokens; 
 
+    printf("Enter math example below:\n");
+
     do{
         c=getC();
         if(isdigit(c)){
@@ -78,8 +80,8 @@ int parser(void)
 
             if(c=='.'||c==','){// for real numbers
                 c=getC();
+                weight=0.1;
                 do{
-                    weight=0.1;
                     num = num+(c-'0')*weight;
                     weight*=0.1;
                     c=getC();
@@ -100,7 +102,7 @@ int parser(void)
         } else printf("\nError: there is no symbol '%c';\n", c);
     }while(c!='='&&c!='\n');
 
-    printf("Answer: %.2f", solver(&tokens, &numIndx));
+    printf("Your answer is %.2f", solver(&tokens, &numIndx));
 
 //    printf("%d\n%d\n%c", tokens.numbers[0], tokens.numbers[1], tokens.operators[0]);
 
@@ -111,5 +113,8 @@ int main(void)
 {
     parser();
 
+    printf("\n\nPress any button to close the window.");
+    int c;      while ((c = getchar()) != '\n' && c != EOF) { }     // to clear input buffer stdin
+    getchar();      // to wait till the button is pressed
     return 0;
 }
